db92f57b86ec17df2ba174ef9cbbaaab998d5468
AL3XEY <allemagne7@hotmail.fr>
2016-07-07 11:01:11 +0200

    Add attempt to detect sharp zones using Octave / Matlab + resource images

--- poc/fourier.m
+++ poc/fourier.m
@@ -1,16 +1,44 @@
 clear all;
 
+full = 0;
+
+%graphics_toolkit('fltk')
+graphics_toolkit('gnuplot')
+
 %haar_test03()
 %pause;
 
-%img = imread("01.jpg");
-img = imread("02.jpg");
-%img = imread("IMG_7291.jpg");
-%img = imread("IMG_7293-e1292255522923.jpg");
+%img = imread('img/01.jpg');
+%img = imread('img/02.jpg');
+%img = imread('img/kidsblur.ppm');
+%img = imread('img/IMG_7291.jpg');
+%img = imread('img/IMG_7293-e1292255522923.jpg');
+%img = imread('img/test1.jpg');
+%img = imread('img/test2.jpg'); % error: rgb2gray: the input must either be an RGB image or a colormap
+%img = imread('img/test3.jpg');
+%img = imread('img/test4.jpg'); %very good
+%img = imread('img/test5.jpg'); %very good
+img = imread('img/test6.jpg'); %very good
+
+%img = imread('img/blur1.jpg');
+%img = imread('img/blur2.jpg');
+%img = imread('img/blur3.jpg');
+%img = imread('img/blur4.jpg');
+
+%img = imread('img/res1.jpg');
+%img = imread('img/res2.jpg');
+%img = imread('img/res3.jpg');
+%img = imread('img/res4.jpg');
+%img = imread('img/res5.jpg');
+%img = imread('img/res6.jpg');
+
 
 % convert image to grayscale
 
 imgGray = rgb2gray(img);
+%imgGray = img; %if image is already grayscaled
+[h,w]=size(img)
+w=w/3
 
 % perform Fourier transform
 IMG = fft2(img);
@@ -19,28 +47,166 @@ IMG = abs(IMG);
 IMG = log(IMG+1);
 IMG = mat2gray(IMG);
 
-laplacian = zeros(3,3);
-laplacian(1,2) = -1;
-laplacian(2,1) = -1;
-laplacian(2,3) = -1;
-laplacian(3,2) = -1;
-laplacian(2,2) = 4
+laplacian = zeros(3);
+laplacian(1,2) = 1;
+laplacian(2,1) = 1;
+laplacian(2,3) = 1;
+laplacian(3,2) = 1;
+laplacian(2,2) = -4;
 
-%laplacian = fspecial("laplacian")
-%laplacian = fspecial("laplacian",0.1)
+%laplacian = ones(5);
+%laplacian(3,3) = 8;
+%laplacian(2,3) = -3;
+%laplacian(4,3) = -3;
+%laplacian(3,2) = -3;
+%laplacian(3,4) = -3;
+
+%laplacian = fspecial('laplacian');
+%laplacian = fspecial('laplacian',0.1);
+
+laplacian
 
 res = conv2(imgGray, laplacian);
 res = mat2gray(res);
+%res = uint8(255 * mat2gray(res))
+%res = histeq(res);
 
-% display the image in both temporal and frequential
+if full==0
+	sx = zeros(h,w);
+	sy = zeros(h,w);
+	sxy = zeros(h,w);
+	dx = zeros(h,w);
+	dy = zeros(h,w);
+	dxy = zeros(h,w);
 
-figure(1);
-imshow(img);
-figure(2);
+	for i=1:h-1%211%h
+		for j=1:w-1%329%w-1
+			sx(i,j)=(imgGray(i,j)+imgGray(i+1,j))/2;
+			dx(i,j)=imgGray(i+1,j)-imgGray(i,j);
+		end
+	end
+
+	for i=1:h-1%211%h-1
+		for j=1:w-1%329%w
+			sy(i,j)=(imgGray(i,j)+imgGray(i,j+1))/2;
+			dy(i,j)=imgGray(i,j+1)-imgGray(i,j);
+		end
+	end
+
+	for i=1:h-1%211%h-1
+		for j=1:w-1%329%w-1
+			sxy(i,j)=(imgGray(i,j)+imgGray(i+1,j+1))/2;
+			dxy(i,j)=imgGray(i+1,j+1)-imgGray(i,j);
+		end
+	end
+
+	sx = mat2gray(sx);
+	sy = mat2gray(sy);
+	sxy = mat2gray(sxy);
+	dx = mat2gray(abs(dx));
+	dy = mat2gray(abs(dy));
+	dxy = mat2gray(abs(dxy));
+end
+
+tau = 2;
+f = 1/1000000000;
+x0 = 1;
+y0 = 1;
+%for x=1:50%h-1
+	%for y=1:50%w-1
+		%stftx(x,y) = 0;
+		%for u=1:212%h-1
+			%for v=1:330%w-1
+				%stftx(x,y) = stftx(x,y) + double(imgGray(u,v))*exp(-2*pi*i*f*(u*x*x0 + v*y*y0));
+
+				%stftx(x,y) = stftx(x,y) + imgGray(u,v).*hann(tau)*exp(-2*pi*i*(u*x*x0 + v*y*y0));
+			%end
+		%end
+	%end
+%end
+%figure;
+%colormap jet;
+%imagesc(stftx);
+
+%stft = integral imgGray.*hann(tau).*exp(-li)
+
+
+%figure;
+%imshow(img);
+figure;
 imshow(imgGray);
-figure(3);
-imshow(IMG);
-figure(4);
+%figure;
+%imshow(IMG);
+figure;
 imshow(res);
+
+if full==1
+	figure;
+	%colormap jet;
+	imagesc(sx);
+
+	figure;
+	imagesc(sy);
+
+	figure;
+	imagesc(sxy);
+
+	figure;
+	imagesc(dx);
+
+	figure;
+	imagesc(dy);
+
+	figure;
+	imagesc(dxy);
+
+	figure;
+	imhist(imgGray);
+
+	%figure;
+	%[counts,locations]=imhist(imgGray);
+	%stem(locations,counts);
+
+	figure;
+	imhist(res);
+
+end
+
+[counts,locations]=imhist(imgGray);
+locations
+counts
+var(counts)
+
+var(var(imgGray))
+var(var(res))
+
+edges = edge(res);
+
+figure;
+imshow(edges);
+
+%se = strel('line',90,90);
+%se = offsetstrel('ball',5,5);
+mn = [3 3];
+se = strel('rectangle',mn);
+
+dil = edges;
+for i=1:4
+	dil = imdilate(dil,se);
+end
+%figure;
+%imshow(dil);
+
+for i=1:5
+	dil = imerode(dil,se);
+end
+%figure;
+%imshow(dil);
+
+for i=1:10
+	dil = imdilate(dil,se);
+end
+figure;
+imshow(dil);
 
 pause;
