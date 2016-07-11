2c7f34d374dc8445a7856c2e3ee66f7ddc7576a0
AL3XEY <allemagne7@hotmail.fr>
2016-07-11 16:04:16 +0200

    Update code with image basic fusion + resource image set

--- poc/sharp_detect.m
+++ poc/sharp_detect.m
@@ -1,8 +1,11 @@
-function [ ] = sharp_detect( path, full=0 )
+function [sm] = sharp_detect( path, full=0 )
 %sharp_detect Detects and displays sharp zones in an image
 %   Detects sharp zones using a 3x3 Laplacian filter and an edge detector, then dilations and erosions
 
+    pkg load image;
+
     img = imread(path);
+    %img = img(1:50,1:50);
 
     %convert image into grayscale format if it's not already
     [h,w,c]=size(img)
@@ -22,30 +25,46 @@ function [ ] = sharp_detect( path, full=0 )
     	IMG = mat2gray(IMG);
     end
 
-    %create and apply Laplacian filter
-    laplacian = zeros(3);
-    laplacian(1,2) = 1;
-    laplacian(2,1) = 1;
-    laplacian(2,3) = 1;
-    laplacian(3,2) = 1;
-    laplacian(2,2) = -4;
+    if full==1
+        %create and apply Laplacian filter
+        laplacian = zeros(3);
+        laplacian(1,2) = 1;
+        laplacian(2,1) = 1;
+        laplacian(2,3) = 1;
+        laplacian(3,2) = 1;
+        laplacian(2,2) = -4;
 
-    %laplacian = ones(5);
-    %laplacian(3,3) = 8;
-    %laplacian(2,3) = -3;
-    %laplacian(4,3) = -3;
-    %laplacian(3,2) = -3;
-    %laplacian(3,4) = -3;
+        %laplacian = ones(3);
+        %laplacian(2,2) = -2;
 
-    %laplacian = fspecial('laplacian');
-    %laplacian = fspecial('laplacian',0.1);
+        %laplacian = ones(5);
+        %laplacian(3,3) = 8;
+        %laplacian(2,3) = -3;
+        %laplacian(4,3) = -3;
+        %laplacian(3,2) = -3;
+        %laplacian(3,4) = -3;
 
-    laplacian
+        %laplacian = fspecial('laplacian');
+        %laplacian = fspecial('laplacian',0.1);
 
-    res = conv2(imgGray, laplacian);
-    res = mat2gray(res);
-    %res = uint8(255 * mat2gray(res))
-    %res = histeq(res);
+        %laplacian = ones(9);
+        %laplacian(1,:) = [0 1 1 2 2 2 1 1 0];
+        %laplacian(2,:) = [1 2 4 5 5 5 4 2 1];
+        %laplacian(3,:) = [1 4 5 3 0 3 5 4 1];
+        %laplacian(4,:) = [2 5 3 -12 -24 -12 3 5 2];
+        %laplacian(5,:) = [2 5 0 -24 -40 -24 0 5 2];
+        %laplacian(6,:) = [2 5 3 -12 -24 -12 3 5 2];
+        %laplacian(7,:) = [1 4 5 3 0 3 5 4 1];
+        %laplacian(8,:) = [1 2 4 5 5 5 4 2 1];
+        %laplacian(9,:) = [0 1 1 2 2 2 1 1 0];
+
+        laplacian
+
+        res = conv2(imgGray, laplacian);
+        res = mat2gray(res);
+        %res = uint8(255 * mat2gray(res))
+        %res = histeq(res);
+    end
 
     if full==0
     	sx = zeros(h,w);
@@ -113,13 +132,18 @@ function [ ] = sharp_detect( path, full=0 )
 
     figure;
     imshow(imgGray);
+    title('Grayscale');
 
-    figure;
-    imshow(res);
+    if full==1
+        figure;
+        imshow(res);
+        title('Convolution with Laplacian');
+    end
 
     if full==1
     	figure;
     	imshow(IMG);
+        title('2DFFT');
 
     	figure;
     	%colormap jet;
@@ -142,6 +166,7 @@ function [ ] = sharp_detect( path, full=0 )
 
     	figure;
     	imhist(imgGray);
+        title('Histogram of grayscales');
 
     	%figure;
     	%[counts,locations]=imhist(imgGray);
@@ -149,42 +174,159 @@ function [ ] = sharp_detect( path, full=0 )
 
     	figure;
     	imhist(res);
+        title('Histogram of the image convoluted by a Laplacian filter');
 
     end
 
-    [counts,locations]=imhist(imgGray);
-    locations
-    counts
-    var(counts)
+    if full==1
+        [counts,locations]=imhist(imgGray);
+        locations
+        counts
+        var(counts)
 
-    var(var(imgGray))
-    var(var(res))
-
-    edges = edge(res);
-
-    figure;
-    imshow(edges);
-
-    %se = offsetstrel('ball',5,5);
-    se = strel('square',3);
-
-    dil = edges;
-    for i=1:4
-    	dil = imdilate(dil,se);
+        var(var(imgGray))
+        var(var(res))
     end
+
+    if full==1
+        edges = edge(res); %Sobel
+
+        figure;
+        imshow(edges);
+        title('Sobel filter on the result of the convolution by a Laplacian filter');
+
+        %se = offsetstrel('ball',5,5);
+        se = strel('square',3);
+
+        dil = edges;
+        for i=1:4
+        	dil = imdilate(dil,se);
+        end
+        %figure;
+        %imshow(dil);
+
+        for i=1:5
+        	dil = imerode(dil,se);
+        end
+        %figure;
+        %imshow(dil);
+
+        for i=1:10
+        	dil = imdilate(dil,se);
+        end
+        figure;
+        imshow(dil);
+        title('Result of dilations and erosions applied to se Sobel filtered image');
+    end
+
+    %local variance
+    [h,w]=size(imgGray);
+    v = zeros(h,w);
+    vr = zeros(h,w);
+    vrr = zeros(h,w);
+    vr2 = zeros(h,w);
+    vrr2 = zeros(h,w);
+
+    %for i=2:h-1%211%h-1
+    %    for j=2:w-1%329%w-1
+    %        a = imgGray(i-1:i+1,j-1);
+    %        b = imgGray(i-1:i+1,j+1);
+    %        c = imgGray(i,j-1);
+    %        d = imgGray(i,j+1);
+    %        neighbours = [reshape(a, [1,3]) reshape(b, [1,3]) c d];
+    %        %v(i,j) = var(neighbours);
+    %        v(i,j) = abs(mean(neighbours) - imgGray(i,j));
+    %    end
+    %end
+
+    %fv = [[1/24 1/24 1/24 1/24 1/24] [1/24 1/24 1/24 1/24 1/24] [1/24 1/24 -1 1/24 1/24] [1/24 1/24 1/24 1/24 1/24] [1/24 1/24 1/24 1/24 1/24]]
+    %v = conv2(v, fv);
+
+    for i=3:(h-2)%211%h-1
+        for j=3:(w-2)%329%w-1
+            a = imgGray((i-2):(i+2),(j-2):(j-1));
+            b = imgGray((i-2):(i+2),(j+1):(j+2));
+            c = imgGray(i,(j-2):(j-1));
+            d = imgGray(i,(j+1):(j+2));
+            neighbours = [reshape(a, [1,10]) reshape(b, [1,10]) c d];
+            %v(i,j) = var(neighbours);
+            v(i,j) = abs(mean(neighbours) - imgGray(i,j));
+
+            if full==1
+                vr(i,j) = vr(i,j) + v(i,j);
+                vr(i-2:i+2,j-2:j+2) = vr(i-2:i+2,j-2:j+2) + v(i,j);
+            end
+
+            %if full==1
+                vrr(i-1:i+1,j) = vrr(i-1:i+1,j) + 2*v(i,j);
+                vrr(i,j-1:j+1) = vrr(i,j-1:j+1) + 2*v(i,j);
+                vrr(i,j-2) = vrr(i,j-2) + v(i,j);
+                vrr(i,j+2) = vrr(i,j+2) + v(i,j);
+                vrr(i-2,j) = vrr(i-2,j) + v(i,j);
+                vrr(i+2,j) = vrr(i+2,j) + v(i,j);
+                vrr(i-1,j-1) = vrr(i-1,j-1) + v(i,j);
+                vrr(i-1,j+1) = vrr(i-1,j+1) + v(i,j);
+                vrr(i+1,j-1) = vrr(i+1,j-1) + v(i,j);
+                vrr(i+1,j+1) = vrr(i+1,j+1) + v(i,j);
+            %end
+
+        end
+    end
+
+    %TODO replace loops by call to Octave's conv2()
+
+    %fvr = [[1 1 1] [1 2 1] [1 1 1]]
+    %fvrr = [[-1 -1 0 -1 -1] [-1 0 1 0 -1] [0 1 4 1 0] [-1 0 1 0 -1] [-1 -1 0 -1 -1]]
+    %vr = conv2(v, fvr);
+    %vrr = conv2(v, fvrr);
+
+    if full==1
+        figure;
+        colormap jet;
+        imagesc(v);
+        title('Variance of pixel values in original grayscale image');
+    end
+
+    %v = mat2gray(v);
     %figure;
-    %imshow(dil);
+    %imshow(v);
 
-    for i=1:5
-    	dil = imerode(dil,se);
+    if full==1
+        for i=1:h-1
+            for j=1:w-1
+                if dil(i,j) == 0;
+                    vrr2(i,j) = v(i,j);
+                    vr2(i,j) = v(i,j);
+                else
+                    vrr2(i,j) = vrr(i,j);
+                    vr2(i,j) = vr(i,j);
+                end
+            end
+        end
     end
-    %figure;
-    %imshow(dil);
 
-    for i=1:10
-    	dil = imdilate(dil,se);
+    if full==1
+        figure;
+        imagesc(vr);
+        title('Variance accentuation');
+
+        figure;
+        imagesc(vr2);
+        title('Variance localized accentuation');
     end
-    figure;
-    imshow(dil);
+
+        figure;
+        imagesc(vrr);
+        title('Variance strong accentuation');
+
+    if full==1
+        figure;
+        imagesc(vrr2);
+        title('Variance localized strong accentuation');
+    end
+
+
+
+    sm = vr;
 
 end;
--- poc/testbench.m
+++ poc/testbench.m
@@ -4,6 +4,8 @@
 clear all;
 clc;
 
+pkg load image;
+
 %graphics_toolkit('fltk');
 graphics_toolkit('gnuplot');
 
@@ -18,7 +20,7 @@ full = 0;
 %sharp_detect('img/test2.jpg', full); % error: rgb2gray: the input must either be an RGB image or a colormap %kind of bad
 %sharp_detect('img/test3.jpg', full); %not bad
 %sharp_detect('img/test4.jpg', full); %very good
-sharp_detect('img/test5.jpg', full); %very good
+%sharp_detect('img/test5.jpg', full); %very good %PYRAMID
 %sharp_detect('img/test6.jpg', full); %very good
 %sharp_detect('img/test7.jpg', full); %surprisingly good => noisy original image
 %sharp_detect('img/test8.jpg', full); %very good
@@ -39,5 +41,122 @@ sharp_detect('img/test5.jpg', full); %very good
 %sharp_detect('img/sharp2.jpg', full); %kind of good : clouds + regular walls not detected
 %sharp_detect('img/sharp3.jpg', full); %kind of bad
 %sharp_detect('img/sharp4.jpg', full); %bad : clouds not detected at all
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%imgs(:,:,1) = imread('img/01.pgm');
+%imgs(:,:,2) = imread('img/02.pgm');
+
+%imgsc(:,:,:,1) = imread('img/01.ppm');
+%imgsc(:,:,:,2) = imread('img/02.ppm');
+
+%vrs(:,:,1) = sharp_detect('img/01.ppm', full);
+%vrs(:,:,2) = sharp_detect('img/02.ppm', full);
+
+%%%%%%%%%%
+
+%%img = imread('img/sd/set2/img0.ppm');
+%%imgs(:,:,1) = (img(:,:,1) + img(:,:,2) + img(:,:,3))/3;
+%%img = imread('img/sd/set2/img1.ppm');
+%%imgs(:,:,2) = (img(:,:,1) + img(:,:,2) + img(:,:,3))/3;
+%%img = imread('img/sd/set2/img2.ppm');
+%%imgs(:,:,3) = (img(:,:,1) + img(:,:,2) + img(:,:,3))/3;
+
+imgs(:,:,1) = imread('img/sd/set2/img0.pgm');
+imgs(:,:,2) = imread('img/sd/set2/img1.pgm');
+imgs(:,:,3) = imread('img/sd/set2/img2.pgm');
+imgsc(:,:,:,1) = imread('img/sd/set2/img0.ppm');
+imgsc(:,:,:,2) = imread('img/sd/set2/img1.ppm');
+imgsc(:,:,:,3) = imread('img/sd/set2/img2.ppm');
+vrs(:,:,1) = sharp_detect('img/sd/set2/img0.ppm', full);
+vrs(:,:,2) = sharp_detect('img/sd/set2/img1.ppm', full);
+vrs(:,:,3) = sharp_detect('img/sd/set2/img2.ppm', full);
+
+%%%%%%%%%%
+
+%imgs(:,:,1) = imread('img/sd/set1/img0.pgm');
+%imgs(:,:,2) = imread('img/sd/set1/img1.pgm');
+%imgs(:,:,3) = imread('img/sd/set1/img2.pgm');
+%imgs(:,:,4) = imread('img/sd/set1/img3.pgm');
+%imgsc(:,:,:,1) = imread('img/sd/set1/img0.ppm');
+%imgsc(:,:,:,2) = imread('img/sd/set1/img1.ppm');
+%imgsc(:,:,:,3) = imread('img/sd/set1/img2.ppm');
+%imgsc(:,:,:,4) = imread('img/sd/set1/img3.ppm');
+%vrs(:,:,1) = sharp_detect('img/sd/set1/img0.ppm', full);
+%vrs(:,:,2) = sharp_detect('img/sd/set1/img1.ppm', full);
+%vrs(:,:,3) = sharp_detect('img/sd/set1/img2.ppm', full);
+%vrs(:,:,4) = sharp_detect('img/sd/set1/img3.ppm', full);
+
+[h,w,c] = size(vrs)
+
+for x=1:h
+    for y=1:w
+        [v,p] = max(vrs(x,y,:));
+        res(x,y) = imgs(x,y,p);
+        resc(x,y,:) = imgsc(x,y,:,p);
+    end
+end
+
+figure;
+imshow(res);
+imwrite(res, 'results/res.pgm')
+figure;
+imshow(resc);
+imwrite(resc, 'results/res.ppm')
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%im1 = imread('img/01.jpg');
+%im1 = rgb2gray(im1);
+%im2 = imread('img/01.jpg');
+%im2 = rgb2gray(im2);
+%[u,v,cert] = HierarchicalLK(im1, im2, 3, 1, 1, 1
+
+of=0
+if of==1
+    images(:,:,1) = imread('img/lenat1.pgm');
+    images(:,:,2) = imread('img/lenat2.pgm');
+    alpha = 1;
+    iterations = 1;
+    [Vx,Vy] = OpticalFlow(images,alpha,iterations);
+
+    [h,w,c] = size(images);
+    [x,y] = meshgrid(1:h,1:w);
+    %size(x)
+    %size(y)
+    %u = new(1:n,1:n,1);
+    %size(u)
+    %v = new(1:n,1:n,2);
+    %size(v)
+    figure;
+    Vy = -Vy;
+    quiver(x,y,Vx,Vy);
+    set(gca,'YDir','reverse');  %# This flips the y axis
+    axis equal
+
+
+    %Vx = floor(100*mat2gray(Vx));
+    Vx = floor(h.*Vx./(max(Vx)-min(Vx)))
+    %interest[xxmax+k][yymax+l] =  (byte)((cvalues[xxmax][yymax]-min)*255.0/(max - min));
+    Vy = floor(w.*Vy./(max(Vy)-min(Vy)))
+    %images(10,10,1) = 0;
+    %images(10+Vx(10,10),10+Vy(10,10),2) = 0;
+
+    Vx(10,10)
+
+    images(50,50,1) = 0;
+    images(50+Vx(50,50),50+Vy(50,50),2) = 0;
+
+    %images(20,70,1) = 0;
+    %images(20+Vx(20,70),70+Vy(20,70),2) = 0;
+
+    figure;
+    imshow(images(:,:,1));
+
+    figure;
+    imshow(images(:,:,2));
+
+    size(images)
+end
 
 pause;
